4dbd0614c40c80b7339ee851c71ba1cbb160947a
Markevich Anatoly <markduho0@gmail.com>
2021-01-21 15:22:18 +0300

    Second commit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 $(function(){
-
-
-
+    
+        // preloader 
+          $(window).on('load', function () {
+              let $preloader = $('#p_prldr'),
+                  $svg = $preloader.find('.svg');
+              $preloader.delay(1200).fadeOut('slow');
+            });      
   
 });
